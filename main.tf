18e49f77f260f821cbc8680d9a16b2dba52ee6a8
ricardorompar <ricardo.romero@hashicorp.com>
2024-09-05 09:58:39 +0200

    changed name of workspace

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.65.0"
+    }
+  }
+  cloud {
+    organization = "r2-org"
+    workspaces {
+        name = "app1-prod-eu-east-1"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+module "name" {
+    source = "/module_example"
+    prefix = "APP1"
+    region = var.region
+    address_space = "10.0.0.0/16"
+    subnet_prefix = "10.0.10.0/24"
+    instance_type = "t2.micro"
+    admin_username = "hashicorp"
+    height = "400"
+    width = "600"
+    placeholder = "placebear.com"
+}
