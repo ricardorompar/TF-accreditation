f919157801717d7007109d473f4df716e6a85bd1
ricardorompar <ricardo.romero@hashicorp.com>
2024-09-05 16:51:16 +0200

    new module version

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 
 module "hashicat" {
     source  = "app.terraform.io/r2-org/hashicat/r2"
-    version = "0.0.3"
+    version = "0.0.4"
     # insert required variables here
     prefix = "APP1"
     region = var.region
