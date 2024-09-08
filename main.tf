7694de184a572eb30f9dba7edbbc309b366605b8
ricardorompar <ricardo.romero@hashicorp.com>
2024-09-09 00:22:41 +0200

    new test run changing size

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "hashicat" {
     subnet_prefix = "10.0.10.0/24"
     instance_type = var.instance_type
     admin_username = "hashicorp"
-    height = "600"
+    height = "500"
     width = "600"
     placeholder = "loremflickr.com"
 }
