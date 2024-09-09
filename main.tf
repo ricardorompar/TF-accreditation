47515b04ec9fef8556d69fd2d295186ccb023a73
ricardorompar <ricardo.romero@hashicorp.com>
2024-09-09 15:15:57 +0200

    updated cat version

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 
 module "hashicat" {
     source  = "app.terraform.io/r2-org/hashicat/r2"
-    version = "0.1.2"
+    version = "0.1.3"
     # insert required variables her
     prefix = var.prefix
     region = var.region
